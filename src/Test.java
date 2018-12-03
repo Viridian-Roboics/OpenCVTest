c787a14e0ede8cedf2a90d957b5d6340652a8178
Daniel Lawson <daniellawson9999@gmail.com>
2018-12-02 20:56:44 -0600

    reorganized

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,25 @@
+import com.jmatio.io.*;
+import com.jmatio.common.*;
+import com.jmatio.types.*;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.CvType;
 import java.io.*;
 import java.util.*;
+
 public class Test {
     public static void main(String args[]){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("mat = " + mat.dump());
-
+        File file = new File("src/processedimages.mat");
+        try{
+            MatFileReader reader = new MatFileReader(file);
+            System.out.println(reader.getContent());
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("error");
+        }
 
     }
 }
