966646fa6f786e3c28f21da8bd44380bacc99a6e
Daniel Lawson <daniellawson9999@gmail.com>
2018-12-05 00:56:30 -0600

    test progress

---
+++ src/ProcessData.java
@@ -0,0 +1,47 @@
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProcessData {
+   /*
+   Accepts a file's name, and size of matrix stored as well as if it contains a constant as the first
+   entry. Returns an Array
+    */
+    public static Mat readCSVtoMat(String filename, int size){
+        Mat mat = Mat.zeros(size,1,CvType.CV_64F);
+        File f = new File(filename);
+        try{
+
+            Scanner s = new Scanner(f);
+            int c = 0;
+            while(s.hasNextDouble()){
+                double d = s.nextDouble();
+                mat.put(c,0,d);
+                c++;
+            };
+            s.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return mat;
+    }
+    //following two methods from https://stackoverflow.com/questions/14958643/converting-bufferedimage-to-mat-in-opencv
+    public static Mat BufferedImage2Mat(BufferedImage image) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", byteArrayOutputStream);
+        byteArrayOutputStream.flush();
+        return Imgcodecs.imdecode(new MatOfByte(byteArrayOutputStream.toByteArray()), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
+    }
+    public static BufferedImage Mat2BufferedImage(Mat matrix)throws IOException {
+        MatOfByte mob=new MatOfByte();
+        Imgcodecs.imencode(".png", matrix, mob);
+        return ImageIO.read(new ByteArrayInputStream(mob.toArray()));
+    }
+
+}
--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,62 @@
-import com.jmatio.io.*;
-import com.jmatio.common.*;
-import com.jmatio.types.*;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.CvType;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.*;
 
 public class Test {
-    public static void main(String args[]){
+    public static void main(String args[])throws InterruptedException{
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat = " + mat.dump());
-        File file = new File("src/processedimages.mat");
+        Mat weights = ProcessData.readCSVtoMat("weights.csv",901);
+        BufferedImage image = null;
+        File f = new File("botlogopsd.png");
         try{
-            MatFileReader reader = new MatFileReader(file);
-            System.out.println(reader.getContent());
-        } catch (IOException e) {
-            //e.printStackTrace();
-            System.out.println("error");
+            image = ImageIO.read(f);
+        }catch(IOException e){
+            e.printStackTrace();
         }
+        Mat pixels = null;
+        try {
+            pixels = ProcessData.BufferedImage2Mat(image);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("size: " + pixels.size());
+        System.out.println(Arrays.toString(pixels.get(0,0)));
+        //Mat alt = Imgcodecs.imread("botlogopsd.png");
+        Mat resized = new Mat();
+        Imgproc.resize(pixels,resized,
+                new Size(image.getHeight()/3,image.getWidth()/3),
+                0,0,Imgproc.INTER_AREA);
+        Imgproc.cvtColor(resized,resized,Imgproc.COLOR_RGB2GRAY);
+        BufferedImage resizedImage = null;
+        try{
+            resizedImage = ProcessData.Mat2BufferedImage(resized);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(300,300);
+        JLabel label = new JLabel();
 
+        ImageIcon icon = new ImageIcon(image);
+        label.setIcon(icon);
+        frame.add(label);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Thread.sleep(2000);
+        icon.setImage(resizedImage);
+        frame.repaint();
     }
 }
